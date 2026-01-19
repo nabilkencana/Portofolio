0458f9dc1213fc4e8fd7514aecf9aa7adc7b2602
Nabilkencana22 <mohammad_kencana_33rpl@student.smktelkom-mlg.sch.id>
2026-01-19 18:06:12 +0700

    memperbaiki signin github

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,11 +1,24 @@
 import { auth } from "./firebase";
-import { GoogleAuthProvider, GithubAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { GoogleAuthProvider, GithubAuthProvider, signInWithPopup, signOut, signInWithRedirect } from "firebase/auth";
+import { getRedirectResult } from "firebase/auth";
+
+useEffect(() => {
+    getRedirectResult(auth)
+        .then((result) => {
+            if (result?.user) {
+                console.log("Login GitHub sukses:", result.user);
+            }
+        })
+        .catch((error) => {
+            console.error("Redirect error:", error);
+        });
+}, []);
 
 const googleProvider = new GoogleAuthProvider();
 const githubProvider = new GithubAuthProvider();
 
 export const loginWithGoogle = () => signInWithPopup(auth, googleProvider);
 
-export const loginWithGithub = () => signInWithPopup(auth, githubProvider);
+export const loginWithGithub = () => signInWithRedirect(auth, githubProvider);
 
 export const logout = () => signOut(auth);

8aca85fdb32461237bc7090c6ad5dbab0d55cca0
nabilkencana <nabilkencana20@gmail.com>
2026-02-02 22:51:33 +0700

    perbaikan model gemini-2.5-flash

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -59,7 +59,7 @@ const ChatBot = () => {
 
       const genAI = new GoogleGenerativeAI(apiKey);
       const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-flash",
+        model: "gemini-2.5-flash",
         systemInstruction: botContext,
       });
 
